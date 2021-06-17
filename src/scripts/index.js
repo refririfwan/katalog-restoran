94c937241356120b0095315b35d704711ab884b5
Refri Rifwan <refririfwan@gmail.com>
2021-06-17 20:23:38 +0700

    Konfigurasi Drawer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,11 @@
 import 'regenerator-runtime';
 import '../styles/style.css';
 import '../styles/responsive.css';
+import App from './views/app';
 
-// eslint-disable-next-line no-console
-console.log('Hello Coders!');
+// eslint-disable-next-line no-unused-vars
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent'),
+});
