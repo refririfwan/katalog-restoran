6e6058b5f1951c7591841f0fa1e36545eb0fbb32
Refri Rifwan <refririfwan@gmail.com>
2021-06-20 08:11:41 +0700

    Membuat Service Worker

---
+++ src/scripts/utils/sw-register.js
@@ -0,0 +1,12 @@
+import runtime from 'serviceworker-webpack-plugin/lib/runtime';
+
+const swRegister = async () => {
+  if ('serviceWorker' in navigator) {
+    await runtime.register();
+    return;
+  }
+  // eslint-disable-next-line no-console
+  console.log('Service worker not supported in this browser');
+};
+
+export default swRegister;
--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -37,6 +38,9 @@ module.exports = {
           to: path.resolve(__dirname, 'dist/'),
         },
       ],
+    }),
+    new ServiceWorkerWebpackPlugin({
+      entry: path.resolve(__dirname, 'src/scripts/sw.js'),
     }),
   ],
 };
