121e6ea242e8a5c9dd2767e702b82d871d532f2c
Refri Rifwan <refririfwan@gmail.com>
2021-06-21 14:24:18 +0700

    Menambahakan Informasi Pada Halaman Detail

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -5,15 +5,25 @@ const createRestaurantDetailTemplate = (restaurant) => `
   <img class="restaurant__poster" src="${CONFIG.BASE_IMAGE_URL + restaurant.pictureId}" alt="${restaurant.name}" />
   <div class="restaurant__info">
   <h3>Information</h3>
-    <h4>Name</h4>
-    <p>${restaurant.city}</p>
+    <h4>Address</h4>
+    <p>${restaurant.address} , Kota ${restaurant.city}</p>
     <h4>Rating</h4>
     <p>${restaurant.rating}</p>
+    <h4>Categories</h4>
+    <p>${restaurant.categories.map((category) => category.name).join(' | ')}</p>
+    <h4>Foods</h4>
+    <p>${restaurant.menus.foods.map((food) => food.name).join(' | ')}</p>
+    <h4>Drinks</h4>
+    <p>${restaurant.menus.drinks.map((drink) => drink.name).join(' | ')}</p>
   </div>
   <div class="restaurant__description">
     <h3>Description</h3>
     <p>${restaurant.description}</p>
   </div>
+  <div class="restaurant__description">
+  <h3>Customer Reviews</h3>
+  <p>${restaurant.customerReviews.map((customer) => customer.review).join(' | ')}</p>
+</div>
 `;
 
 const createRestaurantItemTemplate = (restaurant) => `
