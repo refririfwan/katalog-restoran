46ae0e22937b784b07107673cecd1316675f8dd0
Refri Rifwan <refririfwan@gmail.com>
2021-06-25 14:53:14 +0700

    Fixing Fontawesome Icon for Like / Favorite

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -44,7 +44,7 @@ const createRestaurantItemTemplate = (restaurant) => `
 
 const createLikeButtonTemplate = () => `
   <button aria-label="like this movie" id="likeButton" class="like">
-    <i class="fa fa-heart-o" aria-hidden="true"></i>
+    <i class="fa fa-heart" aria-hidden="true"></i>
   </button>
 `;
 
