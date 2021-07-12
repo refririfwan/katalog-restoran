7768141a75e70d50d98aa3d3954edb87661f4af5
Refri Rifwan <refririfwan@gmail.com>
2021-07-12 13:10:46 +0700

    Jaring pengaman untuk alur Tidak Menyukai Restoran

---
+++ specs/unlikeRestaurantSpec.js
@@ -0,0 +1,72 @@
+/* eslint-disable no-undef */
+import LikeButtonInitiator from '../src/scripts/utils/like-button-initiator';
+import FavoriteRestaurantIdb from '../src/scripts/data/ favoriterestaurant-idb';
+
+const addLikeButtonContainer = () => {
+  document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+};
+
+describe('Unliking A Restaurant', () => {
+  beforeEach(async () => {
+    addLikeButtonContainer();
+    await FavoriteRestaurantIdb.putRestaurant({ id: 1 });
+  });
+
+  afterEach(async () => {
+    await FavoriteRestaurantIdb.deleteRestaurant(1);
+  });
+
+  it('should display unlike widget when the restaurant has been liked', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="unlike this restaurant"]'))
+      .toBeTruthy();
+  });
+
+  it('should not display like widget when the restaurant has been liked', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="like this restaurant"]'))
+      .toBeFalsy();
+  });
+
+  it('should be able to remove liked restaurant from the list', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    document.querySelector('[aria-label="unlike this restaurant"]').dispatchEvent(new Event('click'));
+
+    expect(await FavoriteRestaurantIdb.getAllRestaurants()).toEqual([]);
+  });
+
+  it('should not throw error if the unliked restaurant is not in the list', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    // hapus dulu film dari daftar restaurant yang disukai
+    await FavoriteRestaurantIdb.deleteRestaurant(1);
+
+    // kemudian, simulasikan pengguna menekan widget batal menyukai restaurant
+    document.querySelector('[aria-label="unlike this restaurant"]').dispatchEvent(new Event('click'));
+
+    expect(await FavoriteRestaurantIdb.getAllRestaurants()).toEqual([]);
+  });
+});
